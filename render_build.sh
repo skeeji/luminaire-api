6c761dbc18b89b24f4804a1c9731855eb9ff9db1
skeeji <quentin.luksar@gmail.com>
2025-05-17 17:53:24 +0200

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+export PYTHONUNBUFFERED=1
+export TF_FORCE_GPU_ALLOW_GROWTH=false
+export CUDA_VISIBLE_DEVICES=-1
+
+echo "Vérification des fichiers:"
+ls -la
+
+echo "Démarrage de Gunicorn:"
+gunicorn --workers=1 --timeout=120 --threads=4 --worker-class=gthread wsgi:app
+
+#!/bin/bash
 
 echo "Informations système:"
 python3 --version
