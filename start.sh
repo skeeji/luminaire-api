c6f4210ed9113f321c451e36e7b678386a83d8af
skeeji <quentin.luksar@gmail.com>
2025-05-17 18:20:11 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PYTHONUNBUFFERED=1
+export TF_FORCE_GPU_ALLOW_GROWTH=false
+export CUDA_VISIBLE_DEVICES=-1
+
+echo "V?rification des fichiers:"
+ls -la
+
+echo "D?marrage de Gunicorn:"
+gunicorn --workers=1 --timeout=120 --threads=4 --worker-class=gthread wsgi:app
