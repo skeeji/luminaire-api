b92b6179b287fc66e4c467bcf572e70a7423836f
skeeji <quentin.luksar@gmail.com>
2025-05-18 13:26:43 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 import os
+
+# Forcer l'utilisation du CPU uniquement
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'false'
 
-# Import app après avoir configuré les variables d'environnement
-from app import app
+from app import app  # Import de l'application Flask
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
